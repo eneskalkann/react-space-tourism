9a7c5b352c9ec4fad7795a1e14c98c04e098b5af
eneskalkann <eneskalkan36e@gmail.com>
2022-10-31 00:12:08 +0300

    design-done

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -15,12 +15,25 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
-  color: white;
-  background-color: #000000;
-  margin-top: 25%;
+  justify-content: center;
+  background-image:url("https://images.unsplash.com/photo-1446776877081-d282a0f896e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80");
+  font-size: 42px;
+  background-position: center;
+  height: 100vh;
+  background-repeat: no-repeat;
+  box-sizing: border-box;
+  background-size: cover;
+  h1{
+    font-size: 66px;
+    color: white;
+  }
   .button{
     text-decoration: none;
+    margin: 20px auto;
     color: white;
+    border: 3px solid white;
+    width: 300px;
+    border-radius: 10px;
   }
 `
 
